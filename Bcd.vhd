49d1a2de6e4dc854cefe6a0159fd5989475ed9ff
FSantos <32193238+FlaviaSSantos@users.noreply.github.com>
2018-02-06 11:43:33 +0100

    Update Bcd.vhd

--- Bcd.vhd
+++ Bcd.vhd
@@ -1,5 +1,6 @@
 ---- Description: 
--- This design is used to 
+-- The Bcd design is used to connect the digits of minutes and hour (from slices of the vector count)
+-- to a specific anode in the 7-seg display decoder.
 --
 
 
@@ -10,24 +11,24 @@ use IEEE.NUMERIC_STD.ALL;
 
 entity Bcd is
     Port (     
-    bcd0: in STD_LOGIC_vector(3 downto 0);
-    seg0 : out std_logic_vector(6 downto 0)
+    bcd: in STD_LOGIC_vector(3 downto 0);
+    seg : out std_logic_vector(6 downto 0) --signal goes to 
     );
 end Bcd;
 
 architecture Behavioral of Bcd is
 
 begin
-    seg0 <= "1000000" when (bcd0 = "0000") else
-            "1111001" when (bcd0 = "0001") else
-            "0100100" when (bcd0 = "0010") else
-            "0110000" when (bcd0 = "0011") else
-            "0011001" when (bcd0 = "0100") else
-            "0010010" when (bcd0 = "0101") else
-            "0000010" when (bcd0 = "0110") else
-            "1111000" when (bcd0 = "0111") else
-            "0000000" when (bcd0 = "1000") else
-            "0011000" when (bcd0 = "1001") else 
+    seg <=  "1000000" when (bcd = "0000") else
+            "1111001" when (bcd = "0001") else
+            "0100100" when (bcd = "0010") else
+            "0110000" when (bcd = "0011") else
+            "0011001" when (bcd = "0100") else
+            "0010010" when (bcd = "0101") else
+            "0000010" when (bcd = "0110") else
+            "1111000" when (bcd = "0111") else
+            "0000000" when (bcd = "1000") else
+            "0011000" when (bcd = "1001") else 
             "1100010";
     
 end Behavioral;
